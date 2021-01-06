a5d8bda6522e462303644338c65cd09ba8e0f745
may <mohamed-ali.yousfi@stud.enis.tn>
2021-01-06 20:14:16 +0100

    added deploy config for azure registry, trying the auto deployement

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          <strong>Chlama Rocks !</strong>
           Wohooo my app is finally deployed, Awesome!
           <br/>
           Edit <code>src/App.js</code> and save to reload.
